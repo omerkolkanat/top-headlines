82d3a67905caa09ed995a5748c41e35cf12c2c07
Omer Kolkanat <omerkolkanat@gmail.com>
2019-04-16 09:06:14 +0300

    project created

---
+++ TopHeadlines/TopHeadlines/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TopHeadlines
+//
+//  Created by Omer Kolkanat on 16.04.2019.
+//  Copyright © 2019 Omer Kolkanat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
